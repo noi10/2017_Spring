c0ff2726a87ed8e232e244683ee9c022a33cc79b
noi2010 <gcb624@hotmail.com>
2017-01-12 19:02:40 -0500

    scratch of HW1

--- MATH7340/HelloWorld.R
+++ MATH7340/HelloWorld.R
@@ -3,5 +3,22 @@
 # Author: lenovo
 ###############################################################################
 
+integrand <- function(x) sqrt(x)
+X <- c()
+for (i in 1:20){
+	X[i] <- 2*i
+}
+print(X)
+Y <- rep(0, 20)
+print(Y)
 
-print("Hello World");
+for (k in 1:20){
+	if (k < 12){
+		Y[k] = cos(3*k)	
+	} 
+	else {
+		Y[k] = integrate(integrand, lower = 0, upper = k)$value
+	}
+}
+print(Y)
+

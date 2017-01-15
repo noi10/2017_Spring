8b2b6f97d0d21ae04592381e822fbf5143ece12a
noi2010 <gcb624@hotmail.com>
2017-01-15 12:53:47 -0500

    small change

--- MATH7340/Gu_Module1HW.R
+++ MATH7340/Gu_Module1HW.R
@@ -1,25 +1,25 @@
 # problem 1(a)
 rm(list=ls())
 vec <- c(5,TRUE)
-class(vec)
+print(class(vec))
 
 
 # problem 1(b)
 rm(list=ls())
 x <- 1:4
 y <- 1:2
-x+y
+print(x+y)
 
 
 # problem 1(c)
 rm(list=ls())
 fsin <- function(x) sin(pi*x)
-fsin(1)
+print(fsin(1))
 
 
 # problem 1(d)
 rm(list=ls())
-c(1,2)%*%t(c(1,2))
+print(c(1,2)%*%t(c(1,2)))
 
 
 # problem 1(e)
@@ -32,7 +32,7 @@ f <- function(x){
 	x + g(x)
 }
 z <- 15
-f(3)
+print(f(3))
 
 
 # problem 2
@@ -41,23 +41,20 @@ sum <- 0
 for (i in 1:1000) {
 	sum <- sum + i^2
 }
-sum
+print(sum)
 
 
-# problem 3(a)
+# problem 3
 rm(list=ls())
 X <- c()
 for (i in 1:20){
 	X[i] <- 2*i
 }
-X
+print(X)
 
-# problem 3(b)
-rm(list=ls())
 Y <- rep(0, 20)
-Y
+print(Y)
 
-# problem 3(c)
 integrand <- function(x) sqrt(x)
 for (k in 1:20){
 	if (k < 12){

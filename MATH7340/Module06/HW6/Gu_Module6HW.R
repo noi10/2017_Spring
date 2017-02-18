23c3d7c301ad3e8751736f64c90ad84549082959
noi2010 <gcb624@hotmail.com>
2017-02-17 19:38:17 -0500

    Math7340 HW6

--- MATH7340/Module06/HW6/Gu_Module6HW.R
+++ MATH7340/Module06/HW6/Gu_Module6HW.R
@@ -1,24 +1,51 @@
-n <- 20
-nsim <- 1000
-n.hyp <- 300
-x0.sim<-matrix(NA, ncol=n, nrow=n.hyp)
-p.fdr<-p.bon<-p.sim<-matrix(NA, nrow=nsim, ncol=n.hyp)
-n.true<-3 
-n.fdisc<-n.disc<-rep(NA,nsim) 
-for (i in 1:nsim) { 
-  x0.sim[,]<-rnorm(n*n.hyp) 
-  x0.sim[1:n.true,] <- x0.sim[1:n.true,] + 1 
-  p.sim[i,]<-apply(x0.sim,1,function(x) t.test(x,mu=0)$p.value) 
-  p.bon[i,]<-p.adjust(p.sim[i,],method='bonferroni') 
-  p.fdr[i,]<-p.adjust(p.sim[i,],method='fdr') 
-} 
+# problem 1
+library(multtest)
+data(golub)
+gol.fac <- factor(golub.cl, levels=0:1, labels=c("ALL", "AML"))
 
+# (a)
+t.test(golub[2972, gol.fac=="ALL"], mu=-0.9, alternative = "greater")
 
-n.disc<-apply(p.sim,1,function(x) sum(x<0.05)) 
-n.fdisc<-apply(p.sim[,-(1:n.true)],1,function(x) sum(x<0.05))
+# (b)
+t.test(golub[2972, gol.fac=="ALL"],golub[2972, gol.fac=="AML"])
 
-mean(n.fdisc)
-mean(n.disc-n.fdisc)
+# (c)
+t.test(golub[2972, gol.fac=="ALL"]-golub[2989, gol.fac=="ALL"], alternative = "less")
 
-fdr.tmp<-n.fdisc/n.disc
-fdr.tmp[n.disc==0] <- 0
+# (d)
+res <- golub[2972, gol.fac=="ALL"] < golub[2989, gol.fac=="ALL"]
+binom.test(sum(res), length(res), p=1/2, alternative = "greater")
+
+# (e)
+res <- golub[2972,] < -0.6
+binom.test(sum(res), length(res), p=0.5, alternative = "less")
+
+# (f)
+resALL <- golub[2972, gol.fac=="ALL"] < -0.6
+obsALL <- sum(resALL); nALL <- length(resALL)
+resAML <- golub[2972, gol.fac=="AML"] < -0.6
+obsAML <- sum(resAML); nAML <- length(resAML)
+prop.test( x=c(obsALL, obsAML), n=c( nALL, nAML ), alternative="two.sided")
+
+# problem 2
+# (a)
+2000*0.05
+
+# (b)
+pbinom(89, size=2000, prob=0.05)
+
+# problem 3
+#(a)
+x.sim <- matrix(rnorm(10000*20, mean=3, sd=4), ncol=20)
+tstat <- function(x) (mean(x)-3)/sd(x)*sqrt(length(x))
+tstat.sim <- apply(x.sim, 1, tstat)
+power.sim <- mean(tstat.sim > qt(0.3, df=19) & tstat.sim < qt(0.4, df=19))
+power.sim+c(-1,0,1)*qnorm(0.975)*sqrt(power.sim*(1-power.sim)/10000)
+
+x.sim <- matrix(rnorm(10000*20, mean=3, sd=4), ncol=20)
+tstat <- function(x) (mean(x)-3)/sd(x)*sqrt(length(x))
+tstat.sim <- apply(x.sim, 1, tstat)
+power.sim <- mean(tstat.sim > qt(0.9, df=19) )
+power.sim+c(-1,0,1)*qnorm(0.975)*sqrt(power.sim*(1-power.sim)/10000)
+
+# problem 4

c34fc648ac07be88795ec06776e002b526ad3a0d
noi2010 <gcb624@hotmail.com>
2017-04-14 17:53:47 -0400

    Demo hw13 Math7340

--- MATH7340/Module13/example.R
+++ MATH7340/Module13/example.R
@@ -30,9 +30,30 @@ fit.lgr <- glm(IsB1~., family=binomial(link='logit'), data=data.tr)
 
 library(VGAM)
 pca.iris<-prcomp(iris[,1:4], scale=TRUE)
-Species<-iris$Species
+#Species<-iris$Species
+#data.pca<-pca.iris$x[,1:3]
+#n<-length(Species)
+#iris2.lgr <- vglm(Species~., family=multinomial, data=iris2)
+
+#iris2.svm <- svm(data.pca, Species, type = "C-classification", kernel = "linear")
+
+Species<-iris$Species #response variable with true classes 
 data.pca<-pca.iris$x[,1:3]
 n<-length(Species)
-iris2.lgr <- vglm(Species~., family=multinomial, data=iris2)
+iris2<-data.frame(Species, data.pca)
 
-iris2.svm <- svm(data.pca, Species, type = "C-classification", kernel = "linear")
+mcr.cv.raw<-rep(NA, n) #A vector to save mcr validation 
+for (i in 1:n) { 
+  print(i)
+  fit.lgr <- vglm(Species~., family=multinomial, data=iris2[-i,]) 
+  #fit logistic regression without i-th observation 
+  pred.prob <- predict(fit.lgr, iris2[i,-1], type="response") 
+  print(pred.prob)
+  #get prediction probability 
+  pred <- apply(pred.prob, 1, which.max) #Assign class 
+  pred <- factor(pred, levels=c("1","2","3"), labels=levels(iris2$Species)) 
+  #relabel 1,2,3 by species names 
+  mcr.cv.raw[i]<- mean(pred!=Species[i]) 
+  #check misclassification 
+  } 
+mcr.cv<-mean(mcr.cv.raw)
